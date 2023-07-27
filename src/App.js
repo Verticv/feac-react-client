80b7b9173567389443cfaa9a9e60ba0724059957
동해 <andrew@donghaeui-Macmini.local>
2023-07-27 18:54:52 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import { TypeAnimation } from "react-type-animation";
-import BG from "./assets/bg.mp4";
 import X from "./assets/x.png";
+import BG2 from "./assets/bg2.mp4";
 import music from "./assets/music.mp3";
 import { useEffect, useRef, useState } from "react";
 import { SiOpensea } from "react-icons/si";
 import { PiSpeakerSimpleHighFill, PiSpeakerSimpleXFill } from "react-icons/pi";
+// import { ControlBar, Player } from "video-react";
 import "./utils/slide.css";
 
 function App() {
@@ -32,19 +33,22 @@ function App() {
 
   return (
     <div className="w-screen relative font-vt h-screen wrap">
+      <link rel="stylesheet" href="https://video-react.github.io/assets/video-react.css" />
       <audio id="audio" loop muted autoPlay>
         <source src={music} type="audio/mp3" />
       </audio>
       <div className="w-screen h-16 px-8 flex items-center justify-between fixed z-50 bg-opacity-50 ">
-        <button className=" flex space-x-4 items-center">
-          <p className="text-5xl hover:text-yellow-200">FEAC</p>
+        <div className=" flex space-x-4 items-center">
+          <button>
+            <p className="text-5xl hover:text-yellow-200">FEAC</p>
+          </button>
           <button
             className="rounded-full flex items-center justify-center text-black  hover:text-yellow-200 pt-0.5"
             onClick={() => setIsPlaying(!isPlaying)}
           >
             {isPlaying ? <PiSpeakerSimpleHighFill className="w-6 h-6" /> : <PiSpeakerSimpleXFill className="w-6 h-6" />}
           </button>
-        </button>
+        </div>
         <div className="flex space-x-4 items-center">
           <SiOpensea className="w-8 h-8 hover:text-yellow-200 cursor-pointer" />
           <div className="bg-black w-8 h-8 flex items-center justify-center rounded-full">
@@ -89,33 +93,7 @@ function App() {
               24000,
               `ROADMAP #1`,
               2500,
-              `You know what? Let's not start with elaborate plans. 
-              I'm not some genius team member; I'm just a person who loves art.
-              It's like whenever I'm going through tough times, the thought of creating something makes me excited and happy (even if it's short-lived). 
-              I have so many ideas, maybe because I'm always lost in random thoughts? 
-              But executing those ideas always hits obstacles, and the most common reason is, well, money. 
-              It might sound like an excuse, but I truly believe it. 
-              Um... why am I sharing all this? Have you ever experienced something like this? 
-              Going through thoughts, actions, and failures over and over until all those failures and experiences come together, and bam! 
-              You get that eureka moment. As if all the previous failures were meant for that one thing. 
-              For me, that's FEAC - the F!@#ing Early Adopted Club. 
-              Pretty funny, right? In this ever-evolving world where new technologies and their derivatives emerge, it can be tough to keep up, right? 
-              Well, not really. Just ignore it. But I can't simply ignore my love for creation. 
-              So, I've always been refreshing the connection between my brain and the world. 
-              And then, I heard that Apple is launching Vision Pro, but it's still 2024, so there's time. 
-              I felt like this was a chance given to me by Apple. What changes when you put on glasses? 
-              What can you do with them? And then, it hit me. Let's F!@#ing Early Adopt in the Vision Pro ecosystem. 
-              We may not become Apes or Punks, but it's like doing your holiday homework in advance (if you've never done it, sorry). 
-              I dream of having a super hip artist club in the Vision Pro ecosystem, preparing for the second NFT boom through Vision Pro's mega-hit. 
-              How about it? With AR, we can bring all our works and other artists' works into our space.
-              NFTs are finally doing their thing. We'll be the first! In painting, video, 3D, music, fashion, whatever, we'll be the first through Vision Pro. 
-              Collaborating with each other, showcasing to the world, and if we get the opportunity, we can even host exhibitions together. 
-              In a world driven by technology, we'll lead with art. People with a lot to say can even open their own podcasts. 
-              All through Vision Pro. Doesn't it sound fun? These features are going to be unveiled through the FEAC app. 
-              To make it happen, we just need 4,999 more FEAC members (I'm one already), and it'll all flow smoothly. 
-              It might be just my imagination, but it's exhilarating. 
-              If you're interested in joining my imagination and making it happen together, feel free to reach out. 
-              I can't do this alone; it's impossible. Let's all enjoy this together!`,
+              `You know what? Let's not start with elaborate plans. I'm not some genius team member; I'm just a person who loves art. It's like whenever I'm going through tough times, the thought of creating something makes me excited and happy (even if it's short-lived).  I have so many ideas, maybe because I'm always lost in random thoughts?  But executing those ideas always hits obstacles, and the most common reason is, well, money.  It might sound like an excuse, but I truly believe it.  Um... why am I sharing all this? Have you ever experienced something like this?  Going through thoughts, actions, and failures over and over until all those failures and experiences come together, and bam!  You get that eureka moment. As if all the previous failures were meant for that one thing.  For me, that's FEAC - the F!@#ing Early Adopted Club.  Pretty funny, right? In this ever-evolving world where new technologies and their derivatives emerge, it can be tough to keep up, right?  Well, not really. Just ignore it. But I can't simply ignore my love for creation.  So, I've always been refreshing the connection between my brain and the world.  And then, I heard that Apple is launching Vision Pro, but it's still 2024, so there's time.  I felt like this was a chance given to me by Apple. What changes when you put on glasses?  What can you do with them? And then, it hit me. Let's F!@#ing Early Adopt in the Vision Pro ecosystem.  We may not become Apes or Punks, but it's like doing your holiday homework in advance (if you've never done it, sorry).  I dream of having a super hip artist club in the Vision Pro ecosystem, preparing for the second NFT boom through Vision Pro's mega-hit.  How about it? With AR, we can bring all our works and other artists' works into our space. NFTs are finally doing their thing. We'll be the first! In painting, video, 3D, music, fashion, whatever, we'll be the first through Vision Pro.  Collaborating with each other, showcasing to the world, and if we get the opportunity, we can even host exhibitions together.  In a world driven by technology, we'll lead with art. People with a lot to say can even open their own podcasts.  All through Vision Pro. Doesn't it sound fun? These features are going to be unveiled through the FEAC app.  To make it happen, we just need 4,999 more FEAC members (I'm one already), and it'll all flow smoothly.  It might be just my imagination, but it's exhilarating.  If you're interested in joining my imagination and making it happen together, feel free to reach out.  I can't do this alone; it's impossible. Let's all enjoy this together!`,
             ]}
             wrapper="span"
             speed={65}
@@ -124,16 +102,34 @@ function App() {
         </div>
       </div>
       <div className="w-full flex justify-center fixed z-10">
+        {/* <div
+          style={{
+            aspectRatio: "1/1",
+            height: "100svh",
+            maxWidth: "100svw",
+            width: "100svh",
+            borderRadius: radius,
+            overflow: "hidden",
+          }}
+          className="h-screen w-full"
+        >
+          <Player ref={videoRef} autoPlay muted aspectRatio="1:1">
+            <source src={BG2} />
+            <ControlBar className="opacity-0" disableDefaultControls={true} disableCompletely={true} autoHide={true} />
+          </Player>
+        </div> */}
         <video
           ref={videoRef}
-          playsinline
-          style={{ borderRadius: radius }}
-          src={BG}
-          muted
-          autoPlay
+          playsInline="playsinline"
+          style={{ borderRadius: radius, backgroundColor: "#000", aspectRatio: "1/1" }}
+          src={BG2}
+          muted={true}
+          autoPlay="autoplay"
+          preload="true"
           className="h-screen"
-          loop
-        ></video>
+          loop={true}
+          type="video/mp4"
+        />
       </div>
     </div>
   );
